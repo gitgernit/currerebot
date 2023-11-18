5af9a887945bea9f6b305d64213de31138012064
gitgernit <petr.zakharov.anton@gmail.com>
2023-11-19 02:24:33 +0300

    added secret token

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
-token = '6498931193:AAEzhtnYFw0gV1pXqULxS15THO1ExUtQYn8'
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import SecretStr
+
+
+class Settings(BaseSettings):
+    bot_token: SecretStr
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+
+
+config = Settings()
