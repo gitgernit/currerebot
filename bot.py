783b37c7db50da7af3015f482057e353d2c41424
gitgernit <petr.zakharov.anton@gmail.com>
2023-11-19 02:32:55 +0300

    TRYING TO FIX THIS SHIT

--- bot.py
+++ bot.py
@@ -2,16 +2,18 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
-from config import token
+from config import config
 from funcs import start_handler_func
 
 logging.basicConfig(level=logging.INFO)
-bot = Bot(token=token)
+bot = Bot(token=config.bot_token.get_secret_value())
 dp = Dispatcher()
+
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     await start_handler_func(message)
+
 
 async def main():
     await dp.start_polling(bot)
